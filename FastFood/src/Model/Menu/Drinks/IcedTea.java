aca413c7ed9d7458ee5bb87ddbb344d06ef2b7d5
camber01 <bernabe.cambarihan@gcash.com>
2022-02-28 20:47:09 +0800

    Change menu name

--- FastFood/src/Model/Menu/Drinks/IcedTea.java
+++ FastFood/src/Model/Menu/Drinks/IcedTea.java
@@ -6,7 +6,7 @@ public class IcedTea extends DrinksMenuType{
 
     public IcedTea() {
         this.menuCode = "DR1";
-        this.name = "Pure Leaf";
+        this.name = "Lemon Iced Tea";
         this.description = "Brewed from real leaves for balanced flavor. No artificial sweeteners or colors. " +
                 "Available in several flavor options including lemon, raspberry, sweetened and unsweetened.";
         this.price = 75.00f;
--- FastFood/src/Model/Menu/Drinks/Water.java
+++ FastFood/src/Model/Menu/Drinks/Water.java
@@ -6,7 +6,7 @@ public class Water extends DrinksMenuType {
 
     public Water(){
         this.menuCode = "DR3";
-        this.name = "Wilkins";
+        this.name = "Bottled Water";
         this.description = "The cool, refreshing and fresh drink for rehydration. " +
                 "This is safe to drink specially for babies and elders. ";
         this.price = 50.00f;
